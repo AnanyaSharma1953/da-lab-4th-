363bcda1e484d6e4fe6f03317079683f1bd469bb
Ananya Sharma <156696581+AnanyaSharma1953@users.noreply.github.com>
2025-04-06 16:36:41 +0530

    Create disjoint_ds.cpp

---
+++ disjoint_ds.cpp
@@ -0,0 +1,14 @@
+BFS.cpp:31:15: error: expected identifier before '(' token
+     void union(int u,int v)
+               ^
+BFS.cpp:31:16: error: expected unqualified-id before 'int'
+     void union(int u,int v)
+                ^~~
+BFS.cpp:31:16: error: expected ')' before 'int'
+BFS.cpp: In function 'int main()':
+BFS.cpp:41:8: error: expected unqualified-id before 'union'
+     uf.union(1, 2);
+        ^~~~~
+BFS.cpp:42:8: error: expected unqualified-id before 'union'
+     uf.union(3, 4);
+        ^~~~~
